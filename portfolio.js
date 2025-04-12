9ef4b925cbc999dd37eee1ced94a9f559463ccfc
Ashutosh Vimal <ashutoshvimal22@gmail.com>
2025-04-13 03:38:50 +0530

    Update education, experience, dynamic year of experience

--- portfolio.js
+++ portfolio.js
@@ -1,30 +1,31 @@
 // store current file name and split it on period
 var fileName = document.location.href.match(/[^\/]+$/)[0];
 
-$( function() { // document is ready
-    $( '.sidebar-container' ).load( 'sidebar.html', function() { // load the sidebar snippet html
-        // update selected nav
-        $( 'a[href="' + fileName + '"] div').addClass( 'nav-selected' );
+$(function () {
+    $('.sidebar-container').load('sidebar.html', function () {
+        // Update selected nav
+        $('a[href="' + fileName + '"] div').addClass('nav-selected');
 
-        $( '.mobile-menu' ).on( 'click', function() { // on click for mobile menu bars icon
-            // toggle the navigation
-            $( '.sidebar-nav-container' ).toggle('slow');
-        } );
-    } );
-} );
+        $('.mobile-menu').on('click', function () {
+            // Toggle only the navigation container
+            $('.sidebar-nav-container').toggleClass('active');
+        });
+    });
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+    const start = new Date('2023-01-05');
+    const now = new Date();
+    let experienceYears = now.getFullYear() - start.getFullYear();
+    let months = now.getMonth() - start.getMonth();
+    if (months < 0) {
+        experienceYears--;
+        months += 12;
+    }
+    if(months > 6){
+        experienceYears += 0.5;
+        console.log(months);
+    }
 
-// store current file name and split it on period
-//var fileName = document.location.href.match(/[^\/]+$/)[0];
-//
-//$( function() { // document is ready
-//$( '.sidebar-container' ).load( 'sidebar.html', function() { // load the sidebar snippet html
-//// update selected nav
-//$( 'a[href="' + fileName + '"] div').addClass( 'nav-selected' );
-//
-//$( '.mobile-menu' ).on( 'click', function() { // on click for mobile menu bars icon
-//// toggle the navigation
-//$( '.sidebar-nav-container' ).toggle();
-//} );
-//} );
-//} );
+    document.getElementById("experience-years").textContent = experienceYears + "+";
+});
